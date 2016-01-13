30595aa52e15b6f1e92c447a7b40ff289873ae58
Tasmanian Devil <TasDevilEve@gmail.com>
2016-01-13 22:00:41 +1300

    Added a few comments

--- Hello_world_on_a_LCD/Hello_world_on_a_LCD.ino
+++ Hello_world_on_a_LCD/Hello_world_on_a_LCD.ino
@@ -10,7 +10,9 @@ LiquidCrystal_I2C lcd(0x27, 2, 1, 0, 4, 5, 6, 7, 3, POSITIVE);  // Set the LCD I
 
 void setup()
 {
+  // Setup size of LCD 20 characters and 2 lines
   lcd.begin(20,2); 
+  // Back light on
   lcd.backlight();
 
   lcd.setCursor(0,0);
