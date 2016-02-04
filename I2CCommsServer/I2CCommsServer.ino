aa26f70361f7b0791fcb31afa0f8f4b9d876ca6b
Tasmanian Devil <TasmanianDevilEve@gmail.com>
2016-02-04 21:45:12 +1300

    Added Code for I2C Server and Client

---
+++ I2CCommsServer/I2CCommsServer.ino
@@ -0,0 +1,33 @@
+#include <Wire.h>
+
+void setup()
+{
+  Wire.begin(); 
+}
+
+void loop()
+{
+  Wire.beginTransmission(1);
+  Wire.write('H');
+  Wire.endTransmission();
+ 
+  delay(500);
+  
+  Wire.beginTransmission(1);
+  Wire.write('L');
+  Wire.endTransmission();
+ 
+  delay(500);
+  
+  Wire.beginTransmission(2);
+  Wire.write('H');
+  Wire.endTransmission();
+ 
+  delay(500);
+  
+  Wire.beginTransmission(2);
+  Wire.write('L');
+  Wire.endTransmission();
+ 
+  delay(500);
+}
