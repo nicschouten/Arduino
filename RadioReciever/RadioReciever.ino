9a4e1bb36f99a97118bfeec5a7befdf83f69089e
Tasmanian Devil <TasmanianDevilEve@gmail.com>
2016-02-07 19:08:30 +1300

    uploaded correct code

--- RadioReciever/RadioReciever.ino
+++ RadioReciever/RadioReciever.ino
@@ -10,31 +10,31 @@ byte messageLength = VW_MAX_MESSAGE_LEN; // the size of the message
 
 const int BitPerSec = 2000;
 const int DataPin = 2;
-const int led = 13;
 
 void setup()
 {
-  lcd.begin(20,4); 
-  
-  pinMode(led, OUTPUT); 
-  
+  lcd.begin(20,2); 
+
+  vw_set_ptt_inverted(true);
   vw_setup(BitPerSec); 
   vw_set_rx_pin(DataPin);  
   vw_rx_start(); 
+
+  lcd.setCursor(0,0);
+  lcd.print("Reciever Ready");
 }
 
 void loop()
 {
   if (vw_get_message(message, &messageLength)) // Non-blocking
   {
-    digitalWrite(led, HIGH);
-    //lcd.clear();
+    lcd.setCursor(0,0);
+    lcd.print("Got data");
 
     for (int i = 0; i < messageLength; i++)
     {
       lcd.write(message[i]);
     }
     delay(10);
-    digitalWrite(led, LOW);
   }
 } 
