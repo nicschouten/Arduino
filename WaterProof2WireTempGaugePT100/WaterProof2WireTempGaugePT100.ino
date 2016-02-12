74a75398963151d20d617dd4f18d45d054c30ad5
Tasmanian Devil <TasmanianDevilEve@gmail.com>
2016-02-13 10:37:41 +1300

    Added LCD screen and constants to code

--- WaterProof2WireTempGaugePT100/WaterProof2WireTempGaugePT100.ino
+++ WaterProof2WireTempGaugePT100/WaterProof2WireTempGaugePT100.ino
@@ -1,26 +1,41 @@
+#include <Wire.h> 
+#include <LiquidCrystal_I2C.h>
+
+LiquidCrystal_I2C lcd(0x27, 2, 1, 0, 4, 5, 6, 7, 3, POSITIVE);
+
 const int analogInPin = A0; 
+
+const int SensorValueLow = 463; 
+const int SensorValueDiff = 36; // differance between high and low sensor value
+const int TempValueDiff = 42; // differance between high and low Temp value
+const int TempValueLow = 9;
 
 int sensorValue = 0;
 double Temp = 0;
 
 void setup() 
 {
-  Serial.begin(9600);
+  lcd.begin(20,2); 
+  lcd.backlight();
 }
 
 void loop() 
 {
   sensorValue = analogRead(analogInPin);
-  Temp = sensorValue-486;
-  Temp = Temp/49;
-  Temp = Temp*60.5;
-  Temp = Temp+11.5;
+  Temp = sensorValue-SensorValueLow;
+  Temp = Temp/SensorValueDiff;
+  Temp = Temp*TempValueDiff;
+  Temp = Temp+TempValueLow;
 
-  Serial.print("Sensor = ");
-  Serial.print(sensorValue);
+  lcd.setCursor(0,0);
+  lcd.print("Value = ");
+  lcd.setCursor(7,0);
+  lcd.print(sensorValue);
 
-  Serial.print(" ,Temp = ");
-  Serial.println(Temp);
+  lcd.setCursor(0,1);
+  lcd.print("Temp = ");
+  lcd.setCursor(7,1);
+  lcd.print(Temp);
 
   delay(200);
 }
